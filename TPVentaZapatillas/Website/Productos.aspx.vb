5424e395eded95fe700f679e8470cb9512b0b2e1
hernanjh@gmail.com <hernanjh@gmail.com@cfb24a80-4aa4-118d-57f8-8aee17dc8b5c>
2013-04-23 13:57:17 +0000

    Version Inicial

---
+++ TPVentaZapatillas/Website/Productos.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class Productos
+    Inherits System.Web.UI.Page
+
+End Class
---
+++ TPVentaZapatillas/Website/Site.master.vb
@@ -0,0 +1,26 @@
+﻿
+Partial Class Site
+    Inherits System.Web.UI.MasterPage
+    Protected Sub LnkInicio_Click(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+    Protected Sub LnkProducto_Click(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+    Protected Sub lnkAdmUsuarios_Click(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+    Protected Sub LnkPermisos_Click(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+    Protected Sub lnkBitacora_Click(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+    Protected Sub lnkBackupRestored_Click(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+    Protected Sub lnkSalir_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Response.Redirect("Login.aspx?Logout=true")
+    End Sub
+End Class
+
