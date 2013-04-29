2b78df266dfaaf092b2fd1255a841088fe19108e
adriansc33@gmail.com <adriansc33@gmail.com@cfb24a80-4aa4-118d-57f8-8aee17dc8b5c>
2013-04-29 16:56:29 +0000

    Cambios Para proptotipos de pantallas.Adrian Scarinci.29/04/2013

---
+++ TPVentaZapatillas/Website/Bitacora.aspx.vb
@@ -0,0 +1,56 @@
+﻿
+Partial Class Bitacora
+    Inherits System.Web.UI.Page
+    Public Class PermisoEjemplo
+        Public Evento As String
+        Public Fecha As String
+        Public usuario As String
+    End Class
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            CargarLista()
+        End If
+    End Sub
+    Private Sub CargarLista()
+        Dim ListaEjemplo As New Generic.List(Of PermisoEjemplo)
+        Dim Ej As PermisoEjemplo
+        Dim Ej2 As PermisoEjemplo
+        Dim Ej3 As PermisoEjemplo
+
+        Ej = New PermisoEjemplo
+        Ej.Evento = "Ingreso al sistema"
+        Ej.Fecha = "10/03/2013"
+        Ej.usuario = "Adrian Scarinci"
+        ListaEjemplo.Add(Ej)
+
+
+        Ej2 = New PermisoEjemplo
+        Ej.Evento = "Ingreso al sistema"
+        Ej.Fecha = "10/03/2013"
+        Ej.usuario = "Gonzalo Dominguez"
+        ListaEjemplo.Add(Ej2)
+
+        Ej3 = New PermisoEjemplo
+        Ej3.Evento = "Ingreso al sistema"
+        Ej3.Fecha = "10/03/2013"
+        Ej3.usuario = "Dario Garcia"
+        ListaEjemplo.Add(Ej3)
+
+        gvBitacora.DataSource = ListaEjemplo
+        gvBitacora.DataBind()
+    End Sub
+    Protected Sub gvBitacora_RowDataBound(ByVal sender As Object, ByVal e As GridViewRowEventArgs)
+        If (e.Row.RowType = DataControlRowType.DataRow) Then
+            Dim Ej As PermisoEjemplo = CType(e.Row.DataItem, PermisoEjemplo)
+            Dim lblEvento As Label = CType(e.Row.FindControl("LblEvento"), Label)
+            Dim LblFecha As Label = CType(e.Row.FindControl("LblFecha"), Label)
+            Dim LblUsuario As Label = CType(e.Row.FindControl("LblUsuario"), Label)
+
+            lblEvento.Text = Ej.Evento
+            LblFecha.Text = Ej.Fecha
+            LblUsuario.Text = Ej.usuario
+
+        End If
+    End Sub
+
+End Class
--- TPVentaZapatillas/Website/Productos.aspx.vb
+++ TPVentaZapatillas/Website/Productos.aspx.vb
@@ -1,5 +1,57 @@
-﻿
-Partial Class Productos
+﻿Partial Class Productos
     Inherits System.Web.UI.Page
+    Public Class Ejemplo
+        Public Imagen As String
+        Public Descripcion As String
+        Public Precio As String
+    End Class
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            CargarLista()
+        End If
+    End Sub
+    Private Sub CargarLista()
+        Dim ListaEjemplo As New Generic.List(Of Ejemplo)
+        Dim Ej As Ejemplo
+        Dim Ej2 As Ejemplo
+        Dim Ej3 As Ejemplo
+
+        Ej = New Ejemplo
+        Ej.Imagen = "images/zapa1.jpg"
+        Ej.Descripcion = "Zapatilla1"
+        Ej.Precio = "$2,10"
+        ListaEjemplo.Add(Ej)
+
+        Ej2 = New Ejemplo
+        Ej2.Imagen = "images/zapa2.jpg"
+        Ej2.Descripcion = "Zapatilla2"
+        Ej2.Precio = "$3,20"
+        ListaEjemplo.Add(Ej2)
+
+
+        Ej3 = New Ejemplo
+        Ej3.Imagen = "images/zapa3.jpg"
+        Ej3.Descripcion = "Zapatilla3"
+        Ej3.Precio = "$5,20"
+        ListaEjemplo.Add(Ej3)
+
+        gvShoppingCart.DataSource = ListaEjemplo
+        gvShoppingCart.DataBind()
+    End Sub
+    Protected Sub gvShoppingCart_RowDataBound(ByVal sender As Object, ByVal e As GridViewRowEventArgs)
+        If (e.Row.RowType = DataControlRowType.DataRow) Then
+            Dim Ej As Ejemplo = CType(e.Row.DataItem, Ejemplo)
+
+            Dim lblDescripcion As Label = CType(e.Row.FindControl("LblDescription"), Label)
+            lblDescripcion.Text = Ej.Descripcion
+
+            Dim ImageProducto As Image = CType(e.Row.FindControl("ImgProducto"), Image)
+            ImageProducto.ImageUrl = Ej.Imagen
+
+            Dim LblPrecio As Label = CType(e.Row.FindControl("LblPrecio"), Label)
+            LblPrecio.Text = Ej.Precio
+        End If
+    End Sub
+
 
 End Class
