32ac10dd90ead0c4f36349e21a786494ee8f3c22
adriansc33@gmail.com <adriansc33@gmail.com@cfb24a80-4aa4-118d-57f8-8aee17dc8b5c>
2013-05-20 00:40:58 +0000

    Subida de Cambios de Login (Usuario-Familia-Patente Digito Verificador Horizontal y Vertical

---
+++ TPVentaZapatillas/DAL/DALUsuario.vb
@@ -0,0 +1,129 @@
+﻿Imports BE
+Public Class DALUsuario
+    'Metodo para verificar si usuario existe en base de datos
+    Public Function VerificarUsuario(ByVal UnUsuario As BEUsuario, ByVal usuario As String, ByVal pass As String) As Boolean
+        Dim ds As New DataSet
+        Dim existe As Boolean
+        Dim NombreSp As String
+        Dim ht As New Hashtable
+        NombreSp = "pa_Usuarios_Get"
+        ht.Add("@Usuario", usuario)
+        ht.Add("@Password", pass.GetHashCode)
+        BD.GetData(NombreSp, ds, ht)
+        existe = ds.Tables(0).Rows.Count > 0
+        If existe Then
+            'Cargo las propiedades de la entidad usario con datos.
+            CargarUsuarioDataRow(UnUsuario, ds.Tables(0).Rows(0))
+        End If
+        Return existe
+    End Function
+    'Metodo para obtener todos los usuarios cargados
+    Public Function GetUsuarios() As DataTable
+        Dim ds As New DataSet
+        Dim existe As Boolean
+        Dim NombreSp As String
+        Dim ht As New Hashtable
+        Dim dt As New DataTable
+        NombreSp = "pa_Usuarios_Get"
+        BD.GetData(NombreSp, ds, ht)
+        existe = ds.Tables(0).Rows.Count > 0
+        If existe Then
+            dt = ds.Tables(0)
+        End If
+        Return dt
+    End Function
+    Public Function GetPermisos(ByVal unIdPerfil As Integer) As Generic.List(Of BEPermiso)
+        Dim ds As New DataSet
+        Dim List As New Generic.List(Of BEPermiso)
+        Dim Per As BEPermiso
+        Dim NombreSp As String
+        Dim ht As New Hashtable
+        Try
+            NombreSp = "pa_PerfilPermisos_Get"
+            ht.Add("@IdPerfil", unIdPerfil)
+            BD.GetData(NombreSp, ds, ht)
+            If ds.Tables(0).Rows.Count > 0 Then
+                'Cargo las propiedades de la entidad permiso con datos. Para luego obtener lista de permisos
+                For Each row As DataRow In ds.Tables(0).Rows
+                    Per = New BEPermiso
+                    Per.IdPermiso = row("IdPermiso")
+                    Per.NombrePermiso = row("NombrePermiso")
+                    Per.LinkPermiso = row("LinkPermiso")
+                    List.Add(Per)
+                Next
+            End If
+        Catch ex As Exception
+            'registrar en bitacora
+        End Try
+        Return List
+    End Function
+    Public Sub CargarUsuarioDataRow(ByVal Unusuario As BEUsuario, ByVal UnDataRow As DataRow)
+        With Unusuario
+            .IdUsuario = UnDataRow("IdUsuario")
+            .Nombre = UnDataRow("Nombre")
+            .Apellido = UnDataRow("Apellido")
+            .IdPerfil = UnDataRow("IdPerfil")
+        End With
+    End Sub
+    'Metodo para obtener guardar usuarios
+    Public Function GuardarUsuario(ByVal unUsuario As BEUsuario) As Boolean
+        Dim ht As New Hashtable
+        Dim NombreSp As String
+        Dim result As Boolean = True
+        Dim ParamSalida As Integer
+        'Calculo digito verificador
+        Dim TexttoACalcular As String = ""
+        Dim DaoDv As New DALDV
+        Try
+
+            ht.Add("@IdUsuario", unUsuario.IdUsuario)
+            ht.Add("@Nombre", unUsuario.Nombre)
+            ht.Add("@Apellido", unUsuario.Apellido)
+            ht.Add("@Usuario", unUsuario.Usuario)
+            ht.Add("@Password", unUsuario.Password)
+            ht.Add("@IdPerfil", unUsuario.IdPerfil)
+            ht.Add("@ParamSalidaId", 0)
+            NombreSp = "pa_Usuarios_Save"
+            result = BD.ExecStoredProcedure(NombreSp, ht, True, ParamSalida)
+            If result Then
+                If ParamSalida <> 0 Then
+                    unUsuario.IdUsuario = ParamSalida
+                    'Registrar en bitacora ingreso
+                Else
+                    'Regisrar en biacora modificacion
+                End If
+                TexttoACalcular = DaoDv.ConcatenarValoresPropiedades(unUsuario)
+                DaoDv.AlmacenarDVH("Usuarios", "IdUsuario", unUsuario.IdUsuario, DaoDv.CalcularDVH(TexttoACalcular))
+                DaoDv.AlmacenarDVV("Usuarios")
+            End If
+        Catch ex As Exception
+            result = False
+            Throw ex
+        End Try
+        Return result
+    End Function
+    'Metodo para obtener Eliminar  usuarios
+    Public Function EliminarUsuario(ByVal IdUsuario As Integer, ByVal IdPerfil As Integer) As Boolean
+        Dim ht As New Hashtable
+        Dim NombreSp As String
+        Dim result As Boolean = True
+        'Calculo digito verificador
+        Dim TexttoACalcular As String = ""
+        Dim DaoDv As New DALDV
+        Try
+
+            ht.Add("@IdUsuario", IdUsuario)
+            ht.Add("@IdPerfil", IdPerfil)
+            NombreSp = "pa_Usuarios_Save"
+            result = BD.ExecStoredProcedure(NombreSp, ht)
+            If result Then
+                DaoDv.AlmacenarDVV("Usuarios")
+                DaoDv.AlmacenarDVV("PerfilPermisos")
+            End If
+        Catch ex As Exception
+            result = False
+            Throw ex
+        End Try
+        Return result
+    End Function
+End Class
--- TPVentaZapatillas/Website/Site.master.vb
+++ TPVentaZapatillas/Website/Site.master.vb
@@ -1,15 +1,57 @@
-﻿
+﻿Imports BLL
+Imports BE
 Partial Class Site
     Inherits System.Web.UI.MasterPage
+    'Pagina "Master" Principal utilizada para la configuracion de permisos.Esta pagina va ser heredada por las otras paginas de operaciones
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            ' Valido integridad de datos si no hay asignada pagina principal unicamente . Seria por primera vez
+            If Me.Session("PaginaActual") Is Nothing Then
+                VerificarIntegridadBase()
+            End If
+        End If
+        'En esta funcion se valida la variable de sesion del Usuario logueado para actualizar los permisos cada vez que 
+        'realiza un "PostBack" por las paginas heredadas 
+        If (Me.Session("LoginOK") = "True") Then
+            If Not Me.Session("UsrLogueado") Is Nothing Then
+                'Actualizo el nombre del usuario logueado
+                LblUsr.Text = "Bienvenido  " + CType(Me.Session("UsrLogueado"), BEUsuario).Nombre
+                'Actualizo los permisos del usuario logueado
+                CargarPermisos()
+            Else
+                Me.Session("PaginaActual") = ""
+                lnkSalir.Text = "Salir"
+                'Oculta todos los links(Accesos a pagina por Javascript
+                HideLinks()
+            End If
+        End If
+    End Sub
+    Private Sub VerificarIntegridadBase()
+        Dim Nombretabla As String = ""
+        If Not (DVV.VerificarIntegridadDatos(Nombretabla)) Then 'Lamado a validacion de verificar integridad de datos
+            Me.Session("LoginOK") = "False"
+            'Peticion de cerrado de pagina
+            Me.Response.Write("<script type='text/javascript'>alert('" + "La base de datos esta corrupta en la tabla  " + Nombretabla + " Contactese con administrador " + "');window.close();</script>")
+            Me.Page.ClientScript.RegisterStartupScript(Me.GetType(), "Ocultartodo", "OcultarTodo();", True)
+            lnkSalir.Text = "Salir"
+        Else
+            Me.Session("LoginOK") = "True"
+        End If
+    End Sub
+    Private Sub HideLinks()
+        Me.Page.ClientScript.RegisterStartupScript(Me.GetType(), "OcultarDivs", "OcultarDivs();", True)
     End Sub
     Protected Sub LnkInicio_Click(ByVal sender As Object, ByVal e As EventArgs)
+        'Variable utilizada para indicar la pagina actual en la cual hizo click el usuario
+        Me.Session("PaginaActual") = "Inicio.aspx"
         Response.Redirect("Inicio.aspx")
     End Sub
     Protected Sub LnkProducto_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Me.Session("PaginaActual") = "Productos.aspx"
         Response.Redirect("Productos.aspx")
     End Sub
     Protected Sub lnkAdmUsuarios_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Me.Session("PaginaActual") = "Adminstracion.aspx"
         Response.Redirect("Adminstracion.aspx")
     End Sub
     Protected Sub LnkPermisos_Click(ByVal sender As Object, ByVal e As EventArgs)
@@ -22,10 +64,67 @@ Partial Class Site
         Response.Redirect("BackupRestored.aspx")
     End Sub
     Protected Sub lnkAdmProductos_Click(ByVal sender As Object, ByVal e As EventArgs)
-        Response.Redirect("Adminstracion.aspx")
+        Me.Session("PaginaActual") = "AdministracionProductos.aspx"
+        Response.Redirect("AdministracionProductos.aspx")
+    End Sub
+    Protected Sub LnkUsuarios_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Response.Redirect("Usuarios.aspx")
     End Sub
     Protected Sub lnkSalir_Click(ByVal sender As Object, ByVal e As EventArgs)
-        Response.Redirect("Login.aspx?Logout=true")
+        ' Si ningun usuario esta logueado cierro pagina
+        If Me.Session("UsrLogueado") Is Nothing Then
+            Me.Response.Write("<script type='text/javascript'>window.close();</script>")
+            Exit Sub
+        End If
+        Me.Session("PaginaActual") = "Inicio.aspx"
+        Me.Session("UsrLogueado") = Nothing
+        Me.Session("PermisosUsrLogueado") = Nothing
+        Response.Redirect("Inicio.aspx")
+    End Sub
+    Protected Sub btnIngresar_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Dim UsuarioLogueado As New BEUsuario
+        Dim Usuario As New Usuario
+        Dim IdUsuario As Integer
+        Dim ListPermisos As Generic.List(Of BEPermiso)
+        Try
+            'Luego de las validaciones que se hicieron del lado del cliente . Se valida en base de datos su existencia
+            UsuarioLogueado = Usuario.CargarUsuario(IdUsuario, txtUsuario.Text.Trim, txtContrasenia.Text.Trim)
+            If (IdUsuario > 0) Then
+                ' Si existe el usuario se actualiza la variable de sesion
+                Me.Session("UsrLogueado") = UsuarioLogueado
+                ListPermisos = Usuario.GetPermisos(UsuarioLogueado.IdPerfil)
+                ' Si existe el usuario se cargan los permisos y se actualiza las variables de sesion
+                Me.Session("PermisosUsrLogueado") = ListPermisos
+                CargarPermisos()
+                ' Se actualiza el usuario logueado 
+                LblUsr.Text = "Bienvenido  " + CType(Me.Session("UsrLogueado"), BEUsuario).Nombre
+                'Se actualiza pagina actual
+                Me.Session("PaginaActual") = "Inicio.aspx"
+            Else
+                lblError.Text = "Usuario no habilitado para utilizar el sistema"
+            End If
+        Catch ex As Exception
+            'Registrar en bitacora
+        End Try
+    End Sub
+    Private Sub CargarPermisos()
+        Dim ListPermisos As Generic.List(Of BEPermiso)
+        Dim ArrayPerm As String = ""
+        'Hidden fields utilizados para Mostrar/Ocultar los contenedores(divs) por javascript
+        hdnPermisosUsr.Value = "" ' hidden field para guardar los permisos del usuario
+        hdnPaginaActual.Value = " " ' hidden field para guardar la pagina actual
+        ' Se obtiene los permisos actuales del usuario logueado
+        ListPermisos = CType(Me.Session("PermisosUsrLogueado"), Generic.List(Of BEPermiso))
+
+        'Se recorre los permisos para  actualizar hidden field de permisos
+        For Each P As BEPermiso In ListPermisos
+            ArrayPerm = ArrayPerm + P.LinkPermiso + "|"
+        Next
+        ' Actualizo Hidden field de Permisos de usuario
+        hdnPermisosUsr.Value = ArrayPerm.Substring(0, ArrayPerm.Length - 1)
+        'Actualizo hidden field de pagina actual
+        hdnPaginaActual.Value = Me.Session("PaginaActual")
+        'Realizo a llamada de funcion havascript para Mostrar/Ocultar Divs
+        Page.ClientScript.RegisterStartupScript(Page.GetType, "CargarPermisos", "CargarDivsPermisos();", True)
     End Sub
 End Class
-
